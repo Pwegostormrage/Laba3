16907dde9bf9c4e9618aeb726f380f00e41654c6
321 123 <none>
2020-12-02 14:35:15 +0700

    main program

--- Lab4_2/Program.cs
+++ Lab4_2/Program.cs
@@ -4,9 +4,34 @@ namespace Lab4_2
 {
 	class Program
 	{
+		private static double A, B;
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			while (true)
+			{
+				PrintMenu();
+				DoAction(Console.ReadLine());
+			}
+		}
+
+		static void PrintMenu() =>
+			Console.WriteLine("1: Input A\n2: Input B\n3: +\n4: -\n5: *\n6: /");
+
+		static void DoAction(string input)
+		{
+			switch (input)
+			{
+				case "1":
+				case "2":
+				case "3":
+				case "4":
+				case "5":
+				case "6":
+				default:
+					Console.WriteLine("Unrecognized command.");
+					break;
+			}
 		}
 	}
 }
